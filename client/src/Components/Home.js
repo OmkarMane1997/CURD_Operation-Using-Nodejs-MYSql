982da1204df6bdcb259e6b1df255fdb6a662a246
omkarMane1997 <omii0.mane@gmail.com>
2022-12-16 21:15:54 +0530

    Delete Function created

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -9,52 +9,72 @@ function Home() {
   const getData = async () => {
     try {
       const response = await axios.get(`${URl}/read`);
-      let data =response.data.result;
+      let data = response.data.result;
       // console.log(data)
       setData(data);
     } catch (error) {
       console.error(error);
     }
   };
+
   useEffect(() => {
     getData();
   }, []);
 
+  const deleteHandler = async (id) => {
+    alert(id);
+    try {
+      await axios.patch(`${URl}/delete/${id}`);
+      getData();
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="container">
       <div className="text-center">
         <div className="display-3">User List</div>
       </div>
       <div className="table-responsive">
-      <table className="table">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Name</th>
-      <th>Email</th>
-      <th>Phone</th>
-      <th>designation</th>
-      <th>Action</th>
-    </tr>
-  </thead>
-  <tbody>
-
- {
-  data.map((item,index)=>{
-    return(<tr key={index}>
-      <th scope="row">{item.id}</th>
-      <td>{item.name}</td>
-      <td>{item.email}</td>
-      <td>{item.phone}</td>
-      <td>{item.designation}</td>
-      <td><NavLink to={`/Update/${item.id}`} className='btn btn-outline-warning btn-sm'>Update</NavLink> <button  className='btn btn-outline-danger btn-sm'>Delete</button></td>
-    </tr>)
-  })
- }
-    
-    
-  </tbody>
-</table>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>designation</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <th scope="row">{item.id}</th>
+                  <td>{item.name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                  <td>{item.designation}</td>
+                  <td>
+                    <NavLink
+                      to={`/Update/${item.id}`}
+                      className="btn btn-outline-warning btn-sm"
+                    >
+                      Update
+                    </NavLink>{" "}
+                    <button
+                      onClick={() => deleteHandler(item.id)}
+                      className="btn btn-outline-danger btn-sm"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
