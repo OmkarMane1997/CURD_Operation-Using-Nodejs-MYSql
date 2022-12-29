29c5c7a2fc7d395fcf08ea1b0a6cdcfc1a7cc49d
omkarMane1997 <omii0.mane@gmail.com>
2022-12-29 11:19:33 +0530

    Hosint

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const assert = require("assert");
 const { StatusCodes } = require("http-status-codes");
-const path = require("path");
+// const path = require("path");
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
